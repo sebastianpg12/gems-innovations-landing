2d208aa368ce5de582a9196c02c0fae474595639
Sebastiï¿½n Pulgarï¿½n <sebastian.pulgarin@s1-inconcertcc.com>
2024-09-26 20:50:39 -0500

    update gif to webp

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -82,3 +82,19 @@ testimDots.forEach((dot, index) => {
 });
 
 playSlide(currentSlide);
+
+const grid = document.querySelector('.technologies-grid');
+const items = Array.from(grid.children);
+const totalItems = items.length;
+const gridWidth = grid.scrollWidth; // Ancho total de la grilla
+const containerWidth = document.querySelector('.technologies-carousel').offsetWidth; // Ancho visible del contenedor
+
+// Clonamos los elementos hasta que el ancho total sea suficiente para llenar el carrusel sin dejar huecos
+let totalWidth = gridWidth;
+while (totalWidth < containerWidth * 2) { // Aseguramos que se clonen suficientes ítems
+  items.forEach(item => {
+    const clone = item.cloneNode(true);
+    grid.appendChild(clone);
+    totalWidth += item.offsetWidth + 30; // Ajustamos el totalWidth
+  });
+}
